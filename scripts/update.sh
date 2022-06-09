932a5afd4bc37465c4d36e65e1e5263d3147a9ca
Cayce House <cayce@house.email>
2022-06-08 20:22:25 -0400

    Initial Commit

---
+++ scripts/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -s https://api.github.com/repos/Fabulously-Optimized/fabulously-optimized/releases \
+| grep -m 1 "browser_download_url.*zip" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -O FO.zip -qi - \
+
+packwiz cf import FO.zip
+
+for mod in $(grep -rlw mods -e 'optional'); do
+        rm ${mod}
+done
+
+rm FO.zip
+
+packwiz refresh
