421bd979120b4e23847b4bfa22c14b9978c17738
Cayce House <cayce@caycehouse.com>
2022-08-01 15:10:39 -0400

    purge old mods

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-curl -s https://api.github.com/repos/Fabulously-Optimized/fabulously-optimized/releases \
-| grep -m 1 "browser_download_url.*zip" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -O FO.zip -qi - \
+curl -s https://api.github.com/repos/Fabulously-Optimized/fabulously-optimized/releases |
+        grep -m 1 "browser_download_url.*zip" |
+        cut -d : -f 2,3 |
+        tr -d \" |
+        wget -O FO.zip -qi -
+
+rm -r mods/
 
 packwiz cf import FO.zip
-
-for mod in $(grep -rlw mods -e 'optional'); do
-        rm ${mod}
-done
 
 rm FO.zip
 rm -r resourcepacks/
