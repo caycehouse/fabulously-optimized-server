1a7cccb248025b0a453cc955033ccf764076ca3d
Cayce House <cayce@caycehouse.com>
2023-06-12 11:40:22 -0400

    bump deps

--- scripts/update.sh
+++ scripts/update.sh
@@ -13,25 +13,8 @@ packwiz cf import FO.zip
 rm FO.zip
 rm -r resourcepacks/
 
-# Pulled from https://github.com/itzg/docker-minecraft-server/blob/master/files/cf-exclude-include.json
-packwiz remove carry-on
-packwiz remove defensive-measures
-packwiz remove dynamiclights-reforged
-packwiz remove entity-texture-features-fabric
-packwiz remove entityculling
-packwiz remove extreme-sound-muffler
-packwiz remove ignitioncoil
-packwiz remove irisshaders
-packwiz remove legendary-tooltips
-packwiz remove neat
-packwiz remove nekos-enchanted-books
-packwiz remove not-enough-animations
-packwiz remove oculus
-packwiz remove rubidium
-packwiz remove rubidium-extra
-packwiz remove magnesium-extras
-packwiz remove skin-layers-3d
-packwiz remove textrues-rubidium-options
-packwiz remove torohealth-damage-indicators
+curl --silent https://raw.githubusercontent.com/itzg/docker-minecraft-server/master/files/cf-exclude-include.json | jq -r '.globalExcludes[]' | while read -r mod; do
+        packwiz remove "$mod"
+done
 
 packwiz refresh
